47039ca6bfcd778c4683cc9f561e3d4f6b8e98b5
JSand89 <javier.esand@gmail.com>
2021-10-22 18:53:10 -0500

    commit CORS

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const http =require('http');
 const socketio = require('socket.io');
 const path= require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets')
 
@@ -29,6 +30,9 @@ class Server{
         //deploy public directory
 
         this.app.use( express.static( path.resolve( __dirname, '../public')));
+        
+        //cors
+        this.app.use(cors());
     }
 
         //setting socket
